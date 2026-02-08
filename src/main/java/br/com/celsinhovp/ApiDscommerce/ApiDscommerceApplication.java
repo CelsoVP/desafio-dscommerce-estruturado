e2673d6e8c91210731aeb0bafead77cb843e57b6
Celso Vicente Pereira <celsinhovp1942@gmail.com>
2026-02-08 14:10:54 -0300

    Criado as classes Category e Product

---
+++ src/main/java/br/com/celsinhovp/ApiDscommerce/ApiDscommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.celsinhovp.ApiDscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDscommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/celsinhovp/ApiDscommerce/ApiDscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.celsinhovp.ApiDscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
